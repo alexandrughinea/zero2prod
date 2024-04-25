4062d8e63fd5bad874d000da988d2963e5633ef6
Alex <alexandrughinea.dev+github@gmail.com>
2024-04-25 09:12:20 +0300

    Fix init script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,6 +3,16 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+  echo "psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo "sqlx is not installed."
+  exit 1
+fi
+
 # Check if a custom user has been set, otherwise default to "postgres".
 DB_USER="${POSTGRES_USER:=postgres}"
 
@@ -24,6 +34,19 @@ docker run \
   -e POSTGRES_USER=${DB_USER} \
   -e POSTGRES_PASSWORD=${DB_PASSWORD} \
   -e POSTGRES_DB=${DB_NAME} \
-  -e "${DB_HOST}":5432 \
+  -p "${DB_PORT}":5432 \
   -d postgres \
   postgres -N 1000
+
+#Keep pinging postgres until it is ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    echo "Postgres is still unavailable, sleeping ..."
+    sleep 1
+done
+
+echo "Postgres is up and running on port #{DB_PORT}"
+
+DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+export DATABASE_URL
+sqlx database create
