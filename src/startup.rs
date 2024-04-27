b91c32465b2848593b7d46fdf94150f8eaa008c5
Alex <alexandrughinea.dev+github@gmail.com>
2024-04-28 02:14:18 +0300

    Add logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,8 +1,8 @@
-use std::net::TcpListener;
-
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
+use std::net::TcpListener;
 
 use crate::routes::{health_check, subscriptions};
 
@@ -11,6 +11,8 @@ pub fn run(tcp_listener: TcpListener, connection_pool: PgPool) -> Result<Server,
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are added using the `wrap` method on `App`
+            .wrap(Logger::default())
             // Register the connection as part of the application state .app_data(connection)
             .app_data(connection_pool.clone())
             .route("/health_check", web::get().to(health_check))
